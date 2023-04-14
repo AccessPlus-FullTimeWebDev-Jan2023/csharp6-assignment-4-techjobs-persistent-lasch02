9f17cfb378a4fed184892b82cb848ac66c7fc9c6
Linda Schasch <lasch02@sbcglobal.net>
2023-04-14 16:04:04 -0500

    Part 3 is done

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -9,6 +9,15 @@
 -- integer EmployerId
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
+-- SELECT name
+-- FROM employers
+-- WHERE location = "St. Louis City";
+
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
+-- SELECT skills.skillname, jobs.name
+-- FROM skills
+-- LEFT JOIN jobskills ON skills.id = jobskills.skillsid INNER JOIN jobs on jobskills.jobsid = jobs.id 
+-- WHERE jobskills.jobsid is not null
+-- ORDER BY skillname;
